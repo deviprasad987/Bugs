7ffe7c7e1072d6a4afa941a7084707091de86c2f
deviprasad987 <105635699+deviprasad987@users.noreply.github.com>
2022-11-18 18:17:52 +0530

    Add files via upload

---
+++ MauiApp4/MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		App.Current.MainPage.ShowPopup(new CustomPopup());
+
+    }
+}
+
